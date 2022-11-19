92d3009ebd26261664f4886bb081c5010c42c0ef
Rajashekar Gunaganti <gunagantirajashekar@gmail.com>
2022-11-19 11:38:55 +0530

    some changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = () => {
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="experience">Experience</a></li>
-        <li><a href="portfolio">Portfolio</a></li>
-        <li><a href="qualification">Qualification</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#qualification">Qualification</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
         <a href="https://github.com/RAJASHEKARGUNAGANTI"><AiFillGithub /></a>
